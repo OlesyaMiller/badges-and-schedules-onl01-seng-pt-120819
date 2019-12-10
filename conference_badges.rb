a6c970026442cc82de73fe17d488de7d171bb51e
Olesya Miller <olesyam.miller@gmail.com>
2019-12-10 22:58:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,6 @@ def batch_badge_creator(array_of_names)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_object([]) { 
-    |name, index| "Hello, #{name}! You have been assigned to room #{index + 1}!"
-  }
+  new_array = []
+  speakers.each_with_index { |name, index| "Hello, #{name}! You have been assigned to room #{index + 1}!"}
 end
