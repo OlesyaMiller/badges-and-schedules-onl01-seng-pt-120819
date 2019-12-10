201b6c7eaf68ac9ea417cf2da101535c7e5a90d4
Olesya Miller <olesyam.miller@gmail.com>
2019-12-10 23:13:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,7 @@ def assign_rooms(speakers)
   speakers.each_with_index.map { |name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
 end
 
-def printer
-  
+def printer(speakers)
+  batch_badge_creator(speakers)
+  assign_rooms(speakers)
 end
